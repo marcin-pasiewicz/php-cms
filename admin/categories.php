3031827372ad09133c95b575666d1ab9c202d980
marcin-pasiewicz <marcin.pasiewicz@gmail.com>
2018-06-18 20:12:15 +0200

    admin panel posts table

--- admin/categories.php
+++ admin/categories.php
@@ -11,8 +11,8 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Blank Page
-                            <small>Subheading</small>
+                            Welcome to admin
+                            <small>author</small>
                         </h1>
                         <div class="col-xs-6">
                             <?php
